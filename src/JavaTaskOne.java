0671a9deb4f059114fe5d1a2f2d8e3dac35a83b2
wujot <wojciech.wujot"gmail.com>
2018-01-10 23:27:03 +0100

    I created new project, ignored unnecessary files, set new repository, created main method, print a message, add to follow not ignored files and set first commit.

---
+++ src/JavaTaskOne.java
@@ -0,0 +1,9 @@
+public class JavaTaskOne {
+
+    // main method
+    public static void main(String[] args){
+
+        // print message
+        System.out.print("Ucze\nsie\nprogramowania");
+    }
+}
